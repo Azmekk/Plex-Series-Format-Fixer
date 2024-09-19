2a8711c48fcfeb07102d03b5d2022b80e809a838
Azmek <68204002+Azmekk@users.noreply.github.com>
2024-09-20 01:47:01 +0300

    Added src folder and made console improvements.

---
+++ src/PlexSeriesNameFormatFixer/Program.cs
@@ -0,0 +1,56 @@
+﻿using PlexSeriesNameFormatFixer.DataObjects;
+using PlexSeriesNameFormatFixer.Helpers;
+using Spectre.Console;
+
+string path = AnsiConsole.Prompt(new TextPrompt<string>("[red]Folder path (leave empty to use relative):[/]"));
+
+if(string.IsNullOrEmpty(path))
+{
+    path = "./";
+}
+
+path = Path.GetFullPath(path);
+
+PathHelper pathHelper = new();
+
+List<string> directories = [path];
+
+AnsiConsole.Status()
+    .Spinner(Spinner.Known.Star)
+    .Start("[yellow]Locating subdirectories...[/]", ctx =>
+    {
+        directories.AddRange(pathHelper.GetAllSubdirectories(path));
+    });
+
+AnsiConsole.MarkupLine("[green]Located subdirectories.[/]");
+
+using FileStream fs = File.Create(Path.Join(path, $"Rename_Log_{DateTime.UtcNow.ToString("yyyy-MM-ddTHH-mm-ssZ")}.txt"));
+using StreamWriter logWriter = new(fs);
+logWriter.WriteLine("The following files were renamed: ");
+
+List<string> files = [];
+EpisodeHelper episodeHelper = new();
+
+AnsiConsole.Status()
+    .Spinner(Spinner.Known.Star)
+    .Start("[yellow]Renaming files...[/]", ctx =>
+    {
+        foreach(var dir in directories)
+        {
+            List<string> dirFiles = pathHelper.GetFiles(dir);
+
+            foreach(string filePath in dirFiles)
+            {
+                string oldName = Path.GetFileName(filePath);
+                EpisodeInfo epInfo = episodeHelper.GetNormalizedEpisode(oldName);
+
+                if(!string.IsNullOrEmpty(epInfo.Name))
+                {
+                    File.Move(filePath, Path.Join(dir, epInfo.Name));
+                    logWriter.WriteLine($"{oldName} -> {epInfo.Name}");
+                }
+            }
+        }
+    });
+
+AnsiConsole.MarkupLine("[green]Renamed files.[/]");
